2ed30927348d64f29a36beeee08c3d90dd617e5b
raph <delens.raphael@outlook.be>
2024-05-11 17:54:20 +0200

    Add transport description

--- EnviPlay/Assets/Scripts/LimitSceneScript.cs
+++ EnviPlay/Assets/Scripts/LimitSceneScript.cs
@@ -22,7 +22,7 @@ public class LimitSceneScript : MonoBehaviour
         switch(gameManager.selectionChoice)
         {
             case "transport":
-                description.text = "transport";
+                description.text = "Cette catégorie évalue différents modes de transport en se basant sur l’émission de gCO2 par individu en France. Ce calcul prend en compte les émissions directes, la phase de vie des véhicules (de leur fabrication à leur démantèlement) ainsi que la production et la distribution de carburant et d’électricité. Cependant, la construction des infrastructures telles que les routes, les voies ferrées et les aéroports n’est pas prise en compte.";
                 break;
             case "location":
                 description.text = "Vous avez choisi la catégorie Lieux, dès lors, vous allez comparer les émissions de CO2 émises par habitant de différents lieux tels que des villes, des pays, ...";
