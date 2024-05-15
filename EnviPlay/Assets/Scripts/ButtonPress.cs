fc19e0a1dd54a1adcfa58373806b7b8302a813de
raph <delens.raphael@outlook.be>
2024-05-15 18:14:58 +0200

    Change arrow color

--- EnviPlay/Assets/Scripts/ButtonPress.cs
+++ EnviPlay/Assets/Scripts/ButtonPress.cs
@@ -37,7 +37,7 @@ public class ButtonPress : MonoBehaviour
     {
         //Etat de la partie
         distanceDeplacement = (Screen.width)/2;
-        vitesseDeplacement = distanceDeplacement/1.6f;
+        vitesseDeplacement = distanceDeplacement/1f;
 
         gameManager = FindObjectOfType<GameManager>();
         if (gameManager != null)
